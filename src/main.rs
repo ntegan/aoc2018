7805583b61404c34cbe268dc03ad24a26642ffab
ntegan <ntegan1@gmail.com>
2020-11-30 21:13:13 -0600

    Add claim to Claim struct parsing for day 3
    Beginning of  day 3 part 1.
    take a list like
    
    ```
    ```
    
    and convert to
    ```
    [
        Claim {
            id: 1,
            inches_from_left: 1,
            inches_from_top: 3,
            width_inches: 4,
            height_inches: 4,
        },
        Claim {
            id: 2,
            inches_from_left: 3,
            inches_from_top: 1,
            width_inches: 4,
            height_inches: 4,
        },
        Claim {
            id: 3,
            inches_from_left: 5,
            inches_from_top: 5,
            width_inches: 2,
            height_inches: 2,
        },
    ]
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,104 @@
 mod input;
 mod myerror;
 
-#[derive(Debug)]
-struct StringPair(String, String);
+//// DAY 3
+////
+// each elf has made a claim about which area would be ideal for santa's suit.
+// all claims have an id and consist of a single rectangle with edges
+// parallel to the edges of the fabric.
+//
+// Each claim's rectangle defined as follows:
+//  * num inches b/t left edge of fabric and left edge of rectangle
+//  * num inches b/t top edge of fabric and top edge of rectangle
+//  * width of rect in inches
+//  * height of rect in inches
+//
+// claim like `#123 @ 3,2: 5x4` means claim id 123 represents 3 from left,...
+// i.e. it represents the #'s here
+// ...........
+// ...........
+// ...#####...
+// ...#####...
+// ...#####...
+// ...#####...
+// ...........
+// ...........
+// ...........
+//
+// Problem is that many of claims overlap.
+// => 2 or more claims may cover part of the same areas.
+//
+// if elves proceed with plans, none will have enouh fabric.
+// how many square inches of fabric are within two or more claims?
 
-fn get_possible_pairs(box_id_list: Vec<&str>) -> Vec<StringPair> {
-    let mut result = Vec::<StringPair>::new();
-    for id in box_id_list.iter() {
-        for id2 in box_id_list.iter() {
-            if *id != *id2 {
-                result.push(StringPair(String::from(*id), String::from(*id2)));
-            }
+mod fabric {
+    use regex::Regex;
+    const FABRIC_STRING_REGEX: &'static str = "^#*([0-9]*) @ ([0-9]*),([0-9]*): ([0-9]*)x([0-9]*)$";
+
+    #[derive(Debug)]
+    pub struct Claim {
+        id: u64,
+        inches_from_left: u64,
+        inches_from_top: u64,
+        width_inches: u64,
+        height_inches: u64,
+    }
+
+    impl Claim {
+        pub fn from_string(string: &String) -> Result<Claim, Box<dyn std::error::Error>> {
+            let re = Regex::new(FABRIC_STRING_REGEX)?;
+
+            let caps = re
+                .captures(string)
+                .ok_or("No regex matches; invalid claim string")?;
+
+            let id = caps
+                .get(1)
+                .ok_or("Couldn't get id group")?
+                .as_str()
+                .parse::<u64>()?;
+            let inches_from_left = caps
+                .get(2)
+                .ok_or("Couldn't get left in group")?
+                .as_str()
+                .parse::<u64>()?;
+            let inches_from_top = caps
+                .get(3)
+                .ok_or("Couldn't get top in group")?
+                .as_str()
+                .parse::<u64>()?;
+            let width_inches = caps
+                .get(4)
+                .ok_or("Couldn't get width group")?
+                .as_str()
+                .parse::<u64>()?;
+            let height_inches = caps
+                .get(5)
+                .ok_or("Couldn't get height group")?
+                .as_str()
+                .parse::<u64>()?;
+
+            Ok(Claim {
+                id,
+                inches_from_left,
+                inches_from_top,
+                width_inches,
+                height_inches,
+            })
         }
     }
-    result
 }
 
-/// test if a pair of strings differ by exactly n characters
-fn string_pair_differs_by_n_characters(pair: &StringPair, n: u64) -> bool {
-    let mut number_of_different_characters = 0;
-
-    for char_pair in pair.0.chars().zip(pair.1.chars()) {
-        if char_pair.0 != char_pair.1 {
-            number_of_different_characters = number_of_different_characters + 1;
-        }
+fn claim_list_from_claim_string_list(
+    claim_string_list: Vec<&str>,
+) -> Result<Vec<fabric::Claim>, Box<dyn std::error::Error>> {
+    let mut result = Vec::new();
+    for claim_string in claim_string_list {
+        let claim = fabric::Claim::from_string(&String::from(claim_string))?;
+        result.push(claim);
     }
 
-    number_of_different_characters == n
-}
-
-fn find_pairs_differing_by_n_characters(
-    box_id_list: Vec<&str>,
-    n: u64,
-) -> Result<StringPair, Box<dyn std::error::Error>> {
-    assert!(n > 0);
-
-    let possible_pairs = get_possible_pairs(box_id_list);
-
-    for pair in possible_pairs {
-        if string_pair_differs_by_n_characters(&pair, n) {
-            return Ok(pair);
-        }
-    }
-
-    return Err(Box::new(myerror::MyError));
-}
-
-fn remove_differing_chars(pair: StringPair) -> String {
-    let mut result = String::new();
-    for i in pair.0.chars().zip(pair.1.chars()) {
-        if i.0 == i.1 {
-            result.push(i.0);
-        }
-    }
-    result
+    Ok(result)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -61,13 +106,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let input = input::read_until_eof()?;
 
-    let box_ids: Vec<&str> = input.lines().collect();
+    let claim_string_list: Vec<&str> = input.lines().collect();
 
-    let result = find_pairs_differing_by_n_characters(box_ids, 1)?;
+    let claim_list = claim_list_from_claim_string_list(claim_string_list)?;
 
-    let result = remove_differing_chars(result);
+    println!("{:#?}", claim_list);
 
-    println!("{}", result);
 
     Ok(())
 }
